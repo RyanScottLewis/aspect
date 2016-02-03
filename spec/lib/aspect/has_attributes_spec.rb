548d09d2a88adf3c6136f47db854c1f80d168dc0
Ryan Scott Lewis <ryan@rynet.us>
2016-02-03 17:39:40 -0500

    HasAttributes :getter and :setter can now accept a Hash with :method and :instance_method keys

--- spec/lib/aspect/has_attributes_spec.rb
+++ spec/lib/aspect/has_attributes_spec.rb
@@ -1,13 +1,19 @@
 require "spec_helper"
 
+class TestObject
+  include Aspect::HasAttributes
+
+  def initialize
+    @name = "foobar"
+  end
+end
+
 describe Aspect::HasAttributes do
   describe ".attribute" do
     context "when no options are given" do
       context "and no block is given" do
         let(:instance) do
-          instance_class = Class.new do
-            include Aspect::HasAttributes
-
+          instance_class = Class.new(TestObject) do
             attribute(:name)
           end
 
@@ -15,7 +21,7 @@ describe Aspect::HasAttributes do
         end
 
         it "should define a getter and a setter which uses the argument given to set the instance variable" do
-          expect(instance.name).to eq(nil)
+          expect(instance.name).to eq("foobar")
           instance.name = "foo"
           expect(instance.name).to eq("foo")
         end
@@ -23,9 +29,7 @@ describe Aspect::HasAttributes do
 
       context "and a block is given" do
         let(:instance) do
-          instance_class = Class.new do
-            include Aspect::HasAttributes
-
+          instance_class = Class.new(TestObject) do
             attribute(:name) { |value| value.to_s }
           end
 
@@ -33,7 +37,7 @@ describe Aspect::HasAttributes do
         end
 
         it "should define a getter and a setter which uses the return value of the block to set the instance variable" do
-          expect(instance.name).to eq(nil)
+          expect(instance.name).to eq("foobar")
           instance.name = 123
           expect(instance.name).to eq("123")
         end
@@ -43,25 +47,73 @@ describe Aspect::HasAttributes do
     context "when the :getter option is given" do
       context "and it's truthy" do
         let(:instance) do
-          instance_class = Class.new do
-            include Aspect::HasAttributes
-
+          instance_class = Class.new(TestObject) do
             attribute(:name, getter: true)
           end
 
           instance_class.new
         end
 
-        it "should define a query getter which uses the argument given to set the instance variable" do
-          expect(instance.name).to eq(nil)
+        it "should define the getter" do
+          expect(instance.name).to eq("foobar")
+        end
+
+        context "and it's a Hash" do
+          context "and the :method option is passed" do
+            let(:instance) do
+              instance_class = Class.new(TestObject) do
+                attribute(:name, getter: { method: :capitalize })
+              end
+
+              instance_class.new
+            end
+
+            it "define the getter which calls the given method on the instance variable" do
+              expect(instance.name).to eq("Foobar")
+            end
+          end
+
+          context "and the :instance_method option is passed" do
+            let(:instance) do
+              instance_class = Class.new(TestObject) do
+                attribute(:name, getter: { instance_method: :reversitalize })
+
+                def reversitalize(value)
+                  value.reverse.capitalize
+                end
+              end
+
+              instance_class.new
+            end
+
+            it "define the getter which calls the given instance method, passing the instance variable as the argument" do
+              expect(instance.name).to eq("Raboof")
+            end
+          end
+
+          context "and both the :method and :instance_method options are passed" do
+            let(:instance) do
+              instance_class = Class.new(TestObject) do
+                attribute(:name, getter: { method: :capitalize, instance_method: :exclamationize })
+
+                def exclamationize(value)
+                  "#{value}!"
+                end
+              end
+
+              instance_class.new
+            end
+
+            it "define the getter which calls the given instance method, passing the result of the method called on the instance variable as the argument" do
+              expect(instance.name).to eq("Foobar!")
+            end
+          end
         end
       end
 
       context "and it's falsey" do
         let(:instance) do
-          instance_class = Class.new do
-            include Aspect::HasAttributes
-
+          instance_class = Class.new(TestObject) do
             attribute(:name, getter: false)
           end
 
@@ -69,7 +121,7 @@ describe Aspect::HasAttributes do
         end
 
         it "should not define a getter" do
-          expect(instance.respond_to?(:name)).to eq(false)
+          expect(instance).not_to respond_to(:name)
         end
       end
     end
@@ -78,37 +130,90 @@ describe Aspect::HasAttributes do
       context "and no block is given" do
         context "and it's truthy" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
+            instance_class = Class.new(TestObject) do
               attribute(:name, setter: true)
             end
 
             instance_class.new
           end
 
-          it "should define a query setter which uses the argument given to set the instance variable" do
-            expect(instance.name).to eq(nil)
+          it "should define a setter which uses the argument given to set the instance variable" do
+            expect(instance.name).to eq("foobar")
             instance.name = "foobar"
             expect(instance.name).to eq("foobar")
+          end
+
+          context "and it's a Hash" do
+            context "and the :method option is passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { method: :capitalize })
+                end
+
+                instance_class.new
+              end
+
+              it "should define the setter which calls the given method on the instance variable" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Foobar")
+              end
+            end
+
+            context "and the :instance_method option is passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { instance_method: :reversitalize })
+
+                  def reversitalize(value)
+                    value.reverse.capitalize
+                  end
+                end
+
+                instance_class.new
+              end
+
+              it "should define the setter which calls the given instance method, passing the instance variable as the argument" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Raboof")
+              end
+            end
+
+            context "and both the :method and :instance_method options are passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { method: :capitalize, instance_method: :exclamationize })
+
+                  def exclamationize(value)
+                    "#{value}!"
+                  end
+                end
+
+                instance_class.new
+              end
+
+              it "define the setter which calls the given instance method, passing the result of the method called on the instance variable as the argument" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Foobar!")
+              end
+            end
           end
         end
 
         context "and it's falsey" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
-              attribute(:admin, query: false)
+            instance_class = Class.new(TestObject) do
+              attribute(:name, setter: false)
             end
 
             instance_class.new
           end
 
-          it "should define a getter and a setter which uses the return value of the block to set the instance variable" do
-            expect(instance.admin).to eq(nil)
-            instance.admin = "truthy"
-            expect(instance.admin).to eq("truthy")
+          it "should define a getter and not a setter" do
+            expect(instance).to respond_to(:name)
+            expect(instance).not_to respond_to(:name=)
           end
         end
       end
@@ -116,43 +221,90 @@ describe Aspect::HasAttributes do
       context "and a block is given" do
         context "and it's truthy" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
-              attribute(:moderator, query: true)
-              attribute(:admin, query: true) { |value| @moderator && value }
+            instance_class = Class.new(TestObject) do
+              attribute(:name, setter: true) { |value| value.reverse }
             end
 
             instance_class.new
           end
 
-          it "should define a query getter and a query setter which uses the return value of the block to set the instance variable as a truthy value" do
-            expect(instance.moderator?).to eq(false)
-            expect(instance.admin?).to eq(false)
-            instance.admin = "truthy"
-            expect(instance.admin?).to eq(false)
-            instance.moderator = true
-            instance.admin = "truthy"
-            expect(instance.moderator?).to eq(true)
-            expect(instance.admin?).to eq(true)
+          it "should define a setter which uses the argument given to set the instance variable" do
+            expect(instance.name).to eq("foobar")
+            instance.name = "foobar"
+            expect(instance.name).to eq("foobar")
+          end
+
+          context "and it's a Hash" do
+            context "and the :method option is passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { method: :capitalize })
+                end
+
+                instance_class.new
+              end
+
+              it "should define the setter which calls the given method on the instance variable" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Foobar")
+              end
+            end
+
+            context "and the :instance_method option is passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { instance_method: :reversitalize })
+
+                  def reversitalize(value)
+                    value.reverse.capitalize
+                  end
+                end
+
+                instance_class.new
+              end
+
+              it "should define the setter which calls the given instance method, passing the instance variable as the argument" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Raboof")
+              end
+            end
+
+            context "and both the :method and :instance_method options are passed" do
+              let(:instance) do
+                instance_class = Class.new(TestObject) do
+                  attribute(:name, setter: { method: :capitalize, instance_method: :exclamationize })
+
+                  def exclamationize(value)
+                    "#{value}!"
+                  end
+                end
+
+                instance_class.new
+              end
+
+              it "define the setter which calls the given instance method, passing the result of the method called on the instance variable as the argument" do
+                expect(instance.name).to eq("foobar")
+                instance.name = "foobar"
+                expect(instance.name).to eq("Foobar!")
+              end
+            end
           end
         end
 
         context "and it's falsey" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
-              attribute(:admin, query: false) { "return value" }
+            instance_class = Class.new(TestObject) do
+              attribute(:name, setter: false)
             end
 
             instance_class.new
           end
 
-          it "should define a getter and a setter which uses the return value of the block to set the instance variable" do
-            expect(instance.admin).to eq(nil)
-            instance.admin = "anything"
-            expect(instance.admin).to eq("return value")
+          it "should define a getter and not a setter" do
+            expect(instance).to respond_to(:name)
+            expect(instance).not_to respond_to(:name=)
           end
         end
       end
@@ -162,9 +314,7 @@ describe Aspect::HasAttributes do
       context "and no block is given" do
         context "and it's truthy" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
+            instance_class = Class.new(TestObject) do
               attribute(:admin, query: true)
             end
 
@@ -180,9 +330,7 @@ describe Aspect::HasAttributes do
 
         context "and it's falsey" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
+            instance_class = Class.new(TestObject) do
               attribute(:admin, query: false)
             end
 
@@ -200,9 +348,7 @@ describe Aspect::HasAttributes do
       context "and a block is given" do
         context "and it's truthy" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
+            instance_class = Class.new(TestObject) do
               attribute(:moderator, query: true)
               attribute(:admin, query: true) { |value| @moderator && value }
             end
@@ -224,9 +370,7 @@ describe Aspect::HasAttributes do
 
         context "and it's falsey" do
           let(:instance) do
-            instance_class = Class.new do
-              include Aspect::HasAttributes
-
+            instance_class = Class.new(TestObject) do
               attribute(:admin, query: false) { "return value" }
             end
 
@@ -245,9 +389,7 @@ describe Aspect::HasAttributes do
 
   describe "#update_attributes" do
     let(:instance) do
-      class_instance = Class.new do
-        include Aspect::HasAttributes
-
+      class_instance = Class.new(TestObject) do
         attr_accessor :name
         attr_accessor :age
       end
@@ -263,7 +405,7 @@ describe Aspect::HasAttributes do
     end
   end
 
-  describe "when included from the method" do
+  context "when included from the method", pending: true do
     context "and the :method option is passed" do
       context "and the values are not nil" do
         let(:instance) do
